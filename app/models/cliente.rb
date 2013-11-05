a34bfa42f4907b3df6ec931cffc7caa66a24b95d
chreyes <chreyes@gmail.com>
2013-11-04 22:12:49 -0300

    ok

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,11 @@
 class Cliente < ActiveRecord::Base
 	has_many :proyectos
+
+	validates_uniqueness_of  :nombre, :email
+	validates_presence_of :nombre, :email
+	validates :email, :presence => true, :uniqueness => true, :email_format => true
+
+	validates_format_of :rut,
+		                      :with => /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\-(k|\d{1})\Z/i,
+				                            :message => "no válido."
 end
