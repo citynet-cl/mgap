c14a96a00a3cc1441e6e35103ac98f18d22c412f
chreyes <chreyes@gmail.com>
2014-02-05 00:25:48 -0300

    v1.0

--- app/controllers/proyectos_cerrados_controller.rb
+++ app/controllers/proyectos_cerrados_controller.rb
@@ -1,4 +1,5 @@
 class ProyectosCerradosController < ApplicationController
+  before_action :set_proyecto_c, only: [:show, :edit, :update, :destroy]
   before_filter :authorize
 	def index
 		@proyectos_c = Proyecto.where(:estado => true)
@@ -12,7 +13,12 @@ class ProyectosCerradosController < ApplicationController
 		@proyecto_c.destroy
 		
 		respond_to do |format|
-			format.html { redirect_to proyectos_url, notice: 'Proyecto anulado.' }
+			format.html { redirect_to proyectos_cerrados_path, notice: 'Proyecto cerrado anulado.' }
 		end
 	end
+
+  private
+    def set_proyecto_c
+      @proyecto_c = Proyecto.find(params[:id])
+    end
 end
